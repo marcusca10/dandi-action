ca5ae379efb282b3bf25f6dd2ae13b9380791b1c
Marcus Carvalho <marcusca@microsoft.com>
2022-04-28 12:39:16 +0200

    sync folders

--- index.js
+++ index.js
@@ -37,7 +37,6 @@ async function run() {
 
 
 
-
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
